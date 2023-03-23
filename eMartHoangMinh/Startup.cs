6410fa79646b0f59e0ee408f9abfca7d13d10300
tientravan <tien.tranvan@vti.com.vn>
2023-03-23 09:30:54 +0700

    create project

---
+++ eMartHoangMinh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eMartHoangMinh.Startup))]
+namespace eMartHoangMinh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
